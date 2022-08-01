0ef6675bcfde8246d0230f91567b884d8bdf468c
unknown <deyasaleh2@gmail.com>
2022-08-01 18:37:09 +0300

    Template 1

--- js/script.js
+++ js/script.js
@@ -11,4 +11,13 @@ function hundleMenu(){
 menu.classList.toggle("is-active");
 action.classList.toggle("is-active");
 }
+ //................................................................................
 
+ const links = document.querySelectorAll(".pointer");
+ links.forEach((item)=>{
+    item.addEventListener("click",()=>
+    {
+        let el = document.getElementById(item.getAttribute("data-link"))
+        el.scrollIntoView({behavior:"smooth",block:"start"});
+    })
+ })
